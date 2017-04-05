e9ce1d5349c0e627a491e2d4657a3634dd717c7d
chris <christophertmartin1993@gmail.com>
2017-04-05 13:05:30 +0100

    initial commit

---
+++ Assets/Scripts/levelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour {
+    string levelName; 
+    GameData 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    levelManager(string levelName)
+    {
+        this.levelName = levelName; 
+    }
+
+   
+
+}
